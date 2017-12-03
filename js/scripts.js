80a24cbb32b064464ae056debeb2509886d42e17
ani <anna.unton@yahoo.com>
2017-12-04 01:51:56 +0700

    initial commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,38 @@
 // scripts.js
+$(function () {  
 
-var button = document.getElementById('get-joke');
-var paragraph = document.getElementById('joke');
-var url = 'http://api.icndb.com/jokes/random';
+	var url = 'https://restcountries.eu/rest/v1/name/';
+	var $countriesList = $('#countries');
 
-getJoke();
+	$('#search').click(searchCountries);
 
-button.addEventListener ('click', function() {
-	getJoke();
+	function searchCountries() {
+		var countryName = $('#country-name').val();
+
+		if(!countryName.length) {
+			countryName = 'Poland';
+		};
+
+		$.ajax ({
+			url: url + countryName,
+			method: 'GET',
+			success: showCountriesList
+		});
+
+	};
+
+	function showCountriesList(response) {
+  		$countriesList.empty();
+  		response.forEach(function(item) {
+        	$('<h3>').text(item.name).appendTo($countriesList);
+        	$('<li>').text('Capital: ' + item.capital).appendTo($countriesList);
+        	$('<li>').text('Timezones: ' + item.timezones).appendTo($countriesList);
+        	$('<li>').text('Domain: ' + item.topLevelDomain).appendTo($countriesList);
+
+    	});
+	}
+
 });
-
-function getJoke() {
-var xhr = new XMLHttpRequest();
-xhr.open ('GET', url); 
-
-xhr.addEventListener('load', function() {
-	var response = JSON.parse(xhr.response);
-	paragraph.innerHTML = response.value.joke;
-});
-
-xhr.send();
-
-};
 
 
 
